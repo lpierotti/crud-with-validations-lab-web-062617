0004024095c8c0cd22b8d7421d7c48f20daf5529
Luke Pierotti <lpierotti2@gmail.com>
2017-07-25 15:41:42 -0400

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,6 +2,15 @@ class Song < ActiveRecord::Base
 	validates :title, presence: true
 	validates :title, uniqueness: { scope: [:artist_name, :released] }
 	validates :artist_name, presence: true
-	validates_with SongValidator
+	validate :release_year_validator
+	
+	def release_year_validator
+		if self.released == true
+			current_year = DateTime.now.year
+			unless self.release_year.present? && self.release_year < current_year
+				self.errors[:release_year] << "Release year is invalid"
+			end
+		end
+	end
 	
 end
